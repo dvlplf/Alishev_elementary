1d823c8a8ee7c6447b6afffa0da9874929f99d4b
dvlplf <dvlplf@gmail.com>
2021-09-05 15:40:36 +0300

    Урок 28. Модификаторы доступа private, default, protected, public

---
+++ learn/src/ru/learn/Module.java
@@ -0,0 +1,16 @@
+package ru.learn;
+
+import Packet1.Person;
+
+public class Module {
+    public int id;
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //public private default protected
+
+        Person person = new Person();
+        System.out.println(person.name);
+
+    }
+}
