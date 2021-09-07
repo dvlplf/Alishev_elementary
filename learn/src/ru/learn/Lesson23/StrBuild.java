4f0f6240eea9eb7b2a6a0cd2cfb4e6aab2452336
dvlplf <dvlplf@gmail.com>
2021-09-07 23:19:53 +0300

    Урок 23: StringBuilder

---
+++ learn/src/ru/learn/Lesson23/StrBuild.java
@@ -0,0 +1,13 @@
+package ru.learn.Lesson23;
+
+import java.util.Locale;
+
+public class StrBuild {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Hello");
+
+        str.append(" my").append(" world");
+        for (int i = 0; i <100; i++) str.append(" ").append(i);
+        System.out.println(str.toString());
+    }
+}
