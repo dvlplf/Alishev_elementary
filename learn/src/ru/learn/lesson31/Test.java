af7c3588048eec0c8660369c2219464665d4390a
dvlplf <dvlplf@gmail.com>
2021-09-11 12:19:29 +0300

    Урок 31: Классы-обертки примитивных типов данных.

---
+++ learn/src/ru/learn/lesson31/Test.java
@@ -0,0 +1,13 @@
+package ru.learn.lesson31;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        short x3 = 1;
+        Short.toString(x3);
+        Integer x2 = 123;
+        Character.toString('a');
+        System.out.println(x + x2);
+
+    }
+}
