d2acf307a8c719f6f4f02ea0d4a8beebf575794f
dvlplf <dvlplf@gmail.com>
2021-09-14 21:49:11 +0300

    Урок 46: Сериализация массивов

--- learn/src/ru/learn/lesson45/ReadObject.java
+++ learn/src/ru/learn/lesson45/ReadObject.java
@@ -1,17 +1,25 @@
 package ru.learn.lesson45;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
         try {
             FileInputStream fis = new FileInputStream("C:\\projects\\Alishev_elementary\\learn\\src\\ru\\learn\\lesson45\\people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
-            Person person1 = (Person) ois.readObject();
-            Person person2  = (Person) ois.readObject();
 
-            System.out.println(person1);
-            System.out.println(person2);
+            //1 способ
+//            int personCount = ois.readInt();
+//            Person[] people = new Person[personCount];
+//            for(int i = 0; i < personCount; i++) {
+//                people[i] = (Person) ois.readObject();
+//            }
+
+            // 2 способ
+            Person[] people = (Person[]) ois.readObject();
+
+            System.out.println(Arrays.toString(people));
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
--- learn/src/ru/learn/lesson45/WriteObject.java
+++ learn/src/ru/learn/lesson45/WriteObject.java
@@ -8,13 +8,24 @@ public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person(10, "Dave");
         Person person2 = new Person(11, "Djon");
+        Person[] people = {
+                new Person(122, "Tom"),
+                new Person(222, "Jerry"),
+                new Person(3, "Bob"),
+                new Person(4, "Jane"),
+        };
         try {
             FileOutputStream fos = new FileOutputStream("C:\\projects\\Alishev_elementary\\learn\\src\\ru\\learn\\lesson45\\people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
+            // 1 способ
+//            oos.writeInt(people.length);
+//            for (Person person:people) {
+//                oos.writeObject(person);
+//            }
 
+            // 2 способ
+            oos.writeObject(people);
             oos.close();
         } catch (IOException e) {
             e.printStackTrace();
