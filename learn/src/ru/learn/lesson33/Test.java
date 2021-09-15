0bed112529edb2bf50cb2bb0f300fc4717d08c86
А336 <dvlplf@gmail.com>
2021-09-15 12:01:40 +0300

    Урок 33: Generics

--- learn/src/ru/learn/lesson33/Test.java
+++ learn/src/ru/learn/lesson33/Test.java
@@ -5,22 +5,27 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+        ///// Java 5 /////
         List animals = new ArrayList();
         animals.add("dog");
         animals.add("cat");
-        animals.add("frog");
-        animals.add("dog");
-
+        animals.add("animal");
         String animal = (String) animals.get(1);
         System.out.println(animal);
 
-        List<String> animals2 = new ArrayList<>();
-        animals2.add("dog");
-        animals2.add("cat");
-        animals2.add("frog");
-        animals2.add("dog");
-        String animal2 = animals2.get(2);
-        System.out.println(animal2);
+        ///// С появлением дженериков //////
+        List<String> animals1 = new ArrayList<String>();
+        animals1.add("dog");
+        animals1.add("dog1");
+        animals1.add("dog2");
+        animals1.add("dog3");
 
+        ////// Java 7 /////
+
+        List<String> animals2 = new ArrayList<String>();
+        animals2.add("dog");
+        animals2.add("dog1");
+        animals2.add("dog2");
+        animals2.add("dog3");
     }
 }
